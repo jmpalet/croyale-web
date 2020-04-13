1347bcde3329d721e1b806728ffc50ac360f51db
Josep Palet <josep.palet@flixbus.com>
2020-04-13 16:03:09 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="secondary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Clash Royale Logo"
+          class="shrink mr-2"
+          src="./assets/logo.png"
+          transition="scale-transition"
+          width="20"
+        />
+        <h1 class="headline mr-4">
+          Clash Royale
+        </h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <span class="mr-1">{{lastWarDate}}</span>
+    </v-app-bar>
+
+    <v-content>
+      <WarLog @updateLastWarDate="lastWarDate = $event"/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import WarLog from './components/WarLog';
+
+export default {
+  name: 'App',
+  components: {
+    WarLog,
+  },
+  data: () => ({
+    lastWarDate: ''
+  }),
+};
+</script>
+
+<style>
+.theme--light.v-application {
+  background-color: #FFF;
+}
+</style>
---
+++ src/components/WarLog.vue
@@ -0,0 +1,198 @@
+<template>
+  <v-container fluid>
+    <v-row class="text-center">
+      <v-col cols="12" align="center">
+        <v-progress-circular
+          :size="50"
+          color="secondary"
+          indeterminate
+          v-if="transformed.length == 0"
+        ></v-progress-circular>
+        <v-card
+          class="player mb-2"
+          v-for="(player, i) in transformed"
+          :key="i"
+          flat
+        >
+          <div class="d-flex justify-space-between">
+            <div class="d-flex">
+              <div class="ranking d-flex align-center ma-2 pa-1">
+                #{{i+1}}
+              </div>
+              <v-card-title
+                class="headline px-0"
+                v-text="player.name"
+              ></v-card-title>
+            </div>
+            <div class="wars d-flex align-center mr-2">
+              <div 
+                v-for="(war, j) in player.wars"
+                :key="j"
+                class="d-flex ml-1 log align-top"
+                v-bind:class="logClass(war)"
+              >
+                <v-badge
+                  bordered
+                  v-if="war.collectionDayBattlesPlayed < 3"
+                  color="warning"
+                  class="ml-2 mt-1"
+                  dot
+                >
+                </v-badge>
+              </div>
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="text-center" v-if="transformed.length > 0">
+      <v-col cols="12" align="center">
+        <v-card
+          class="legend mb-1"
+          min-width="400"
+          max-width="600"
+          v-for="(legendItem, i) in legend"
+          :key="i"
+          flat
+        >
+          <div class="d-flex justify-space-between">
+            <div class="d-flex align-center">
+              <div 
+                class="d-flex log align-top ma-2 pa-1"
+                :class="legendItem.class"
+              >
+                <v-badge
+                  bordered
+                  v-if="legendItem.type == 'badge'"
+                  color="warning"
+                  class="ml-1"
+                  dot
+                >
+                </v-badge>
+              </div>
+              <span>{{legendItem.description}}</span>
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'WarLog',
+
+    data: () => ({
+      error: '',
+      transformed: [],
+      legend: [
+        {
+          'type': 'log',
+          'class': 'all',
+          'description': 'Batalla final i batalles de recol·lecció guanyades'
+        },
+        {
+          'type': 'log',
+          'class': 'win',
+          'description': 'Batalla final guanyada'
+        },
+        {
+          'type': 'log',
+          'class': 'lost',
+          'description': 'Batalla final perduda'
+        },
+        {
+          'type': 'log',
+          'class': 'skipped',
+          'description': 'Batalla final no feta'
+        },
+        {
+          'type': 'log',
+          'class': '',
+          'description': 'No participa a la guerra'
+        },
+        {
+          'type': 'badge',
+          'class': 'none',
+          'description': 'Alguna batalla de recol·lecció no feta'
+        },
+      ]
+    }),
+    methods: {
+      logClass: function (warlog) {
+        if (warlog.won > 0) {
+          if (warlog.maxCollectionPointsReached) {
+            return 'all'
+          }
+          return 'win'
+        }
+        if (warlog.won == 0) {
+          if (warlog.lost > 0) {
+            return 'lost'
+          } else {
+            return 'skipped'
+          }
+        }
+        return null
+      },
+    },
+    created() {
+      axios
+        .get('http://130.211.26.158/clan/2PUGVU8U/warlog')
+        .then(response => {
+          this.transformed = response.data.warlog
+          this.$emit('updateLastWarDate', response.data.lastWarEndDate);
+        })
+        .catch(error => {
+          this.error = error.response
+        })
+    }
+  }
+</script>
+
+<style scoped>
+.v-card.player {
+  background: linear-gradient(to bottom, #F1F1F1 0%,#F1F1F1 50%,#F1F1F1 50%,#E1E1E1 50%,#E1E1E1 100%);
+  /*min-width: 500px;*/
+  max-width: 800px;
+}
+.v-card.player .headline {
+  color: #333;
+  line-height: 0;
+}
+.v-card.player .ranking {
+  background: #444;
+  color: #FFF;
+  font-size: 12px;
+  border-radius: 4px
+}
+.v-card.legend {
+  font-size: 10px;
+  color: #333;
+}
+.log{
+  width: 12px;
+  height: 12px;
+  background: #888;
+  border-radius: 4px;
+}
+.log.win{
+  background: #14BE44;
+}
+.log.lost{
+  background: #D13333;
+}
+.log.all{
+  background: #00A3FF;
+}
+.log.skipped{
+  background: #424242;
+}
+.log.none{
+  background: transparent;
+  border: 1px solid #DDD;
+}
+</style>
